041e6a6cc48054c401afbc1e0b05b5eb377dd525
Nkechi Okeke <janecutie89@gmail.com>
2021-08-16 13:38:55 +0100

    Define Song entity

---
+++ TheBulbTunes/EFDataService/Models/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService.Models
+{
+   public class Song
+    {
+        [Required]
+        public Guid SongId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Artist { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Album { get; set; }
+        
+        [MaxLength(250)]
+        public string Featuring {get; set;}
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
