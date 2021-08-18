8813ef7b70a8b98a48406f1a9b9c6d28633a8287
Nkechi Okeke <janecutie89@gmail.com>
2021-08-18 10:12:47 +0100

    First Upoad

---
+++ TheBulbTunes/EFDataService/TheBulbTunesDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBulbTunes.EFDataService
+{
+    class TheBulbTunesDBContext : DbContext
+    {
+
+        // Constructor to set up the Connection to the DB
+    
+
+        //DBSet properties, one for each entity/model
+
+
+        //OnConfiguring 
+
+
+
+    }
+}
