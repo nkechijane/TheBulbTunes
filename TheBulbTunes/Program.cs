3acfb95570c893e103b133b28ad22e154ffea07a
Nkechi Okeke <janecutie89@gmail.com>
2021-08-19 10:54:08 +0100

    Consume Update method of songService

--- TheBulbTunes/Program.cs
+++ TheBulbTunes/Program.cs
@@ -23,19 +23,30 @@ namespace TheBulbTunes
             //songService.Create("Essence", "Wizkid", "Made In Lagos", "Tems", "R&B", new DateTime(2020, 10, 30));
 
             // Fetch songs that meet some search criteria
-            List<Song> filteredSongs1 = songService.FetchWithFilter("over", "romantic", null, null);
-            List<Song> filteredSongs2 = songService.FetchWithFilter("Ess", "R", "Lagos", "Kid");
+            //List<Song> filteredSongs1 = songService.FetchWithFilter("over", "romantic", null, null);
+            //List<Song> filteredSongs2 = songService.FetchWithFilter("Ess", "R", "Lagos", "Kid");
 
+            // Set the id of song to be updated
+            Guid idOfSongToUpdate1 = new Guid("05e4b13c-1fe2-4ef6-a268-08d96238e7ab"); //Non - existing id
+            Guid idOfSongToUpdate2 = new Guid("d528422f-54eb-407c-b8c6-08d962e9146b"); //existing id
+
+            // Create a song object containing new info for the update
+            Song songWithNewInfo = new Song()
+            {
+                Genre = "Rap/Hip-hop",
+                ReleaseDate = new DateTime(2013, 3, 31)
+            };
+
+            //Call the update mehod to make deseired update
+            songService.Update(idOfSongToUpdate1, songWithNewInfo);
+            songService.Update(idOfSongToUpdate2, songWithNewInfo);
 
 
             // Fetch all songs after delete
             availableSongs = songService.FetchAll();
-            printer(filteredSongs2);
+            printer(availableSongs);
             
         }
-
-
-
 
         public static void printer(List<Song> availableSongs)
         {
@@ -44,7 +55,7 @@ namespace TheBulbTunes
             foreach (Song song in availableSongs)
             {
                 Console.WriteLine();
-                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}");
+                Console.Write($"{song.Title}\t{song.Artist}\t{song.Album}\t{song.Genre}\t{song.ReleaseDate}");
             }
         }
     }
