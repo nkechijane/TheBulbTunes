2b1af22f9a14b60d41bc85ceef9c2c386aa1ca42
Nkechi Okeke <janecutie89@gmail.com>
2021-08-20 16:35:11 +0100

    Create the userservice Class

---
+++ TheBulbTunes/EFDataService/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBulbTunes.EFDataService.Models;
+
+namespace TheBulbTunes.EFDataService.Services
+{
+   public class UserService
+    {
+        private readonly TheBulbTunesDBContext _context = new TheBulbTunesDBContext();
+        private List<Song> _songs;
+
+        //Create a user
+        public string Create(User newUser)
+        {
+            newUser.UserId = new Guid();
+            _context.Users.Add(newUser);
+            var response = _context.SaveChanges();
+
+            if(response > 0)
+            {
+                return "successful insert "+ response + " data";
+            }
+            else
+            {
+                return "Unsuccessful insertion";
+            }
+        }
+
+        public List<User> FetchAll()
+        {
+            return _context.Users.ToList();
+        }
+
+
+
+    }
+}
